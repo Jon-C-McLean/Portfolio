9961a97c190db8e1eb10bbb4bbf15524e4796411
Jon McLean <jon@mclean.one>
2020-07-08 00:41:32 +1000

    Added basic state tracker for experience page

--- src/components/experienceContent.js
+++ src/components/experienceContent.js
@@ -1,52 +1,67 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
+import classNames from "classnames";
 
 import "../styles/main.scss"
 
-const ExperienceContent = () => {
-    return (
-        <section className="list-content">
-            <div className="inner-list">
-                <div className="title"><span className="numbering">02.</span>Experience</div>
-                <div className="intro">
-                    I have worked with several companies as an iOS developer and consultant. Most recently I have worked with CardCastle and Origma in Canberra as a
-                    Senior iOS developer and iOS/Mobile developer and consultant, respectively. 
-                </div>
+class ExperienceContent extends Component {
+    state = {
+        isActive: false
+    };
 
-                <div className="list">
-                    <div className="content">
-                        <ul>
-                            <li>CardCastle</li>
-                            <li>Origma</li>
-                            <li>Freelancing</li>
-                        </ul>
+    handleClick = () => {
+        //this.setState(state => ({ isActive: !state.isActive }));
+    };
+
+    render() {
+        const menuClass = classNames({
+            active: this.state.isActive
+        });
+
+        return (
+            <section className="list-content">
+                <div className="inner-list">
+                    <div className="title"><span className="numbering">02.</span>Experience</div>
+                    <div className="intro">
+                        I have worked with several companies as an iOS developer and consultant. Most recently I have worked with CardCastle and Origma in Canberra as a
+                        Senior iOS developer and iOS/Mobile developer and consultant, respectively. 
                     </div>
-                    <div className="detail-panel">
-                        <div>
-                            <div className="detail-title">Developer <span className="company"><Link to="https://cardcastle.co">@ CardCastle</Link></span></div>
-                            <div className="detail-date">April 2019<span className="hyphon">-</span>Feb 2020</div>
+
+                    <div className="list">
+                        <div className="content">
                             <ul>
-                                <li>Developed major and complex features for iOS application according to given specifications</li>
-                                <li>Reviewed other contributors' additions to the codebase, their merge requests, and provided feedback</li>
-                                <li>Maintained a combined Objective-C/Swift codebase for the iOS application</li>
-                                <li>Developed unit testing automation systems</li>
+                                <li className={menuClass} onClick={this.handleClick}>CardCastle</li>
+                                <li>Origma</li>
+                                <li>Freelancing</li>
                             </ul>
                         </div>
-                        <div hidden="true;">
-                            <div className="detail-title">Developer/Consultant <span className="company"><Link to="https://origma.com.au">@ Origma</Link></span></div>
-                            <div className="detail-date">Nov 2018<span className="hyphon">-</span>Feb 2020</div>
-                            <ul>
-                                <li>Developed major and complex features for iOS application according to given specifications</li>
-                                <li>Provided consulting on all mobile development projects</li>
-                                <li>Maintained all iOS codebases, repositories, and projects</li>
-                                <li>Maintained testing systems for iOS projects</li>
-                            </ul>
+                        <div className="detail-panel">
+                            <div>
+                                <div className="detail-title">Developer <span className="company"><Link to="https://cardcastle.co">@ CardCastle</Link></span></div>
+                                <div className="detail-date">April 2019<span className="hyphon">-</span>Feb 2020</div>
+                                <ul>
+                                    <li>Developed major and complex features for iOS application according to given specifications</li>
+                                    <li>Reviewed other contributors' additions to the codebase, their merge requests, and provided feedback</li>
+                                    <li>Maintained a combined Objective-C/Swift codebase for the iOS application</li>
+                                    <li>Developed unit testing automation systems</li>
+                                </ul>
+                            </div>
+                            <div hidden="true;">
+                                <div className="detail-title">Developer/Consultant <span className="company"><Link to="https://origma.com.au">@ Origma</Link></span></div>
+                                <div className="detail-date">Nov 2018<span className="hyphon">-</span>Feb 2020</div>
+                                <ul>
+                                    <li>Developed major and complex features for iOS application according to given specifications</li>
+                                    <li>Provided consulting on all mobile development projects</li>
+                                    <li>Maintained all iOS codebases, repositories, and projects</li>
+                                    <li>Maintained testing systems for iOS projects</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        );
+    }
 }
 
 export default ExperienceContent
