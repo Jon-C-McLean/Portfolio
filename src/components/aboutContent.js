0152bd078cd8e621e83dff57e4ce56e82efb9f12
Jon McLean <jon@mclean.one>
2020-07-09 01:55:59 +1000

    Rebuilt about page content

--- src/components/aboutContent.js
+++ src/components/aboutContent.js
@@ -5,13 +5,19 @@ import "../styles/main.scss"
 
 const AboutContent = () => {
     return (
-        <section className="pageContent">
-            <div className="inner-content">
-                <div className="title"><span className="numbering">01.</span>About</div>
+        <section className="banner">
+            <div className="inner-banner">
+                <div className="banner-title">I am</div>
+                <div className="keypoint nofade">Jon McLean</div>
+                <div className="banner-title">And I am a</div>
+                <div className="keypoint somefade">Software Engineer</div>
                 <div className="content">
-                    My name is Jon McLean and I am currently a second year software engineering student doing a Bachelor of Engineering (Hons) majoring in Software at the 
+                    I am currently a second year software engineering student doing a Bachelor of Engineering (Hons) majoring in Software at the 
                     University of Technology Sydney. I specialize in developing mobile applications, primarily for iOS, and their respective backend APIs. 
-
+                    <br/>
+                    <br/>
+                    I work on my own projects in my freetime. Currently I am working on developing a polical polling application based of the swiping idea in the
+                    popular dating app Tinder.
                 </div>
             </div>
         </section>
--- src/components/banner.js
+++ src/components/banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
                 </div>
                 <div className="keypoint nofade">Jon McLean</div>
                 <div className="keypoint somefade">I build cool things</div>
-                <div className="content">
+                <div className="content fade">
                     I am software engineering student based in Sydney Australia.
                     I specialize in designing and building mobile applications and backends.
                 </div> 
