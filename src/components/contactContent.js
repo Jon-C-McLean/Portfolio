e85217e246e76a3f3b55447a1caf5046fd0d2a76
Jon McLean <jon@mclean.one>
2020-07-09 02:24:29 +1000

    Added basic contact page

---
+++ src/components/contactContent.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ContactContent = () => {
+    return (
+        <section className="banner">
+            <div className="inner-banner">
+                <div className="banner-title">What do to next</div>
+                <div className="keypoint nofade">Get in contact</div>
+                <div className="content fade">
+                    I am always looking for new and exciting opportunities to explore. My inbox is always open to whoever would like to contact me!
+                    I'll always try my best to get back to you quickly.
+
+                </div>
+
+                <div className="button-container">
+                    <div className="button">
+                        <a href="mailto:jon@mclean.one"><p>Contact Me</p></a>    
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ContactContent
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import ContactContent from "../components/contactContent"
+
+import "../styles/main.scss"
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <ContactContent />
+        </Layout>
+    )
+}
+
+export default ContactPage
