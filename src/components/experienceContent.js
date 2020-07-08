f1d71a6f3b076006322c53cb109a3fdf192d8fe7
Jon McLean <jon@mclean.one>
2020-07-09 01:13:55 +1000

    Changed layout for experienceContent component

--- src/components/experienceContent.js
+++ src/components/experienceContent.js
@@ -4,64 +4,61 @@ import classNames from "classnames";
 
 import "../styles/main.scss"
 
-class ExperienceContent extends Component {
-    state = {
-        isActive: false
-    };
+const ExperienceContent = () => {
+    return (
+        <section className="list-content">
+            <div className="inner-list">
+                <div className="title"><span className="numbering">02.</span>Experience</div>
+                <div className="intro">
+                    I have worked with several companies as an iOS developer and consultant. Most recently I have worked with CardCastle and Origma in Canberra as a
+                    Senior iOS developer and iOS/Mobile developer and consultant, respectively. 
+                </div>
 
-    handleClick = () => {
-        //this.setState(state => ({ isActive: !state.isActive }));
-    };
-
-    render() {
-        const menuClass = classNames({
-            active: this.state.isActive
-        });
-
-        return (
-            <section className="list-content">
-                <div className="inner-list">
-                    <div className="title"><span className="numbering">02.</span>Experience</div>
-                    <div className="intro">
-                        I have worked with several companies as an iOS developer and consultant. Most recently I have worked with CardCastle and Origma in Canberra as a
-                        Senior iOS developer and iOS/Mobile developer and consultant, respectively. 
-                    </div>
-
-                    <div className="list">
-                        <div className="content">
+                <div className="list">
+                    <div className="list-element">
+                        <div className="experienceName">CardCastle</div>
+                        <div className="experienceContent">
+                            <div className="detail-title">Developer <span className="company"><Link to="https://cardcastle.co">@ CardCastle</Link></span></div>
+                            <div className="detail-date">April 2019<span className="hyphon">-</span>Feb 2020</div>
                             <ul>
-                                <li className={menuClass} onClick={this.handleClick}>CardCastle</li>
-                                <li>Origma</li>
-                                <li>Freelancing</li>
+                                <li>Developed major and complex features for iOS application according to given specifications</li>
+                                <li>Reviewed other contributors' additions to the codebase, their merge requests, and provided feedback</li>
+                                <li>Maintained a combined Objective-C/Swift codebase for the iOS application</li>
+                                <li>Developed unit testing automation systems</li>
                             </ul>
                         </div>
-                        <div className="detail-panel">
-                            <div>
-                                <div className="detail-title">Developer <span className="company"><Link to="https://cardcastle.co">@ CardCastle</Link></span></div>
-                                <div className="detail-date">April 2019<span className="hyphon">-</span>Feb 2020</div>
-                                <ul>
-                                    <li>Developed major and complex features for iOS application according to given specifications</li>
-                                    <li>Reviewed other contributors' additions to the codebase, their merge requests, and provided feedback</li>
-                                    <li>Maintained a combined Objective-C/Swift codebase for the iOS application</li>
-                                    <li>Developed unit testing automation systems</li>
-                                </ul>
-                            </div>
-                            <div hidden="true;">
-                                <div className="detail-title">Developer/Consultant <span className="company"><Link to="https://origma.com.au">@ Origma</Link></span></div>
-                                <div className="detail-date">Nov 2018<span className="hyphon">-</span>Feb 2020</div>
-                                <ul>
-                                    <li>Developed major and complex features for iOS application according to given specifications</li>
-                                    <li>Provided consulting on all mobile development projects</li>
-                                    <li>Maintained all iOS codebases, repositories, and projects</li>
-                                    <li>Maintained testing systems for iOS projects</li>
-                                </ul>
-                            </div>
+                    </div>
+
+                    <div className="list-element">
+                        <div className="experienceName">Origma</div>
+                        <div className="experienceContent">
+                            <div className="detail-title">Developer/Consultant <span className="company"><Link to="https://origma.com.au">@ Origma</Link></span></div>
+                            <div className="detail-date">Nov 2018<span className="hyphon">-</span>Feb 2020</div>
+                            <ul>
+                                <li>Developed major and complex features for iOS application according to given specifications</li>
+                                <li>Provided consulting on all mobile development projects</li>
+                                <li>Maintained all iOS codebases, repositories, and projects</li>
+                                <li>Maintained testing systems for iOS projects</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="list-element">
+                        <div className="experienceName">Freelancing</div>
+                        <div className="experienceContent">
+                            <div className="detail-title">Freelancing <span className="company"><Link to="https://upwork.com">@ Upwork</Link></span></div>
+                            <div className="detail-date">Feb 2020<span className="hyphon">-</span>Current</div>
+                            <ul>
+                                <li>Worked for many international clients on a variety of projects sizes</li>
+                                <li>Developed and maintained relationships with clients</li>
+                                <li>Developed iOS, web, and desktop applications under extreme time restrictions</li> 
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    )
 }
 
 export default ExperienceContent
