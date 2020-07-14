afd62324faee0bbf792bc95b8af1919269530865
Jon McLean <jon@mclean.one>
2020-07-14 21:43:47 +1000

    Removed warnings

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {useStaticQuery, graphql} from "gatsby"
 
 import "../styles/main.scss"
 
--- src/components/experienceContent.js
+++ src/components/experienceContent.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "gatsby"
-import classNames from "classnames";
 
 import "../styles/main.scss"
 
